90cdaa528ae1cd26c8e64f4c09cfa44f96dc8a4f
fegger <29882464+fegger@users.noreply.github.com>
2017-08-01 10:30:57 +1000

    Added paths; initialization generalised
    Added path to coordinates CSV file, added path to the directory containing the data directories. Made variable initialization and iteration over data general to accommodate any data size.

--- concatenatePy.py
+++ concatenatePy.py
@@ -11,66 +11,94 @@ from scipy.interpolate import griddata
 from numpy import genfromtxt
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import ImageGrid
-from matplotlib import rc
+#from matplotlib import rc
+#
+#rc('text',usetex=True)
+#rc('font',family='serif')
 
-rc('text',usetex = True)
-rc('font',family = 'serif')
+"Variable Initialization"
+rawData=[]
+avData=[]
 
-height = np.array([60,98,79]) #in mm
-width = np.array([78,110,179,265,307,352,390]) #in mm
-points_x,points_y = np.meshgrid(height,width,sparse = False, indexing = 'ij')
+"Paths to the data"
+path_coordinates='C:\\Users\\uqfegger\\Documents\\Kent_powerDistrib\\_shakerDistribution.csv' #Path to csv file containing the coordinates
+path_data_dirs='C:\\Users\\uqfegger\\Documents\\Kent_powerDistrib\\Batch\\' #Path to the directory containing the data directories
 
-data = os.listdir("/PATH/TO/DATA/data.csv")
-grid_width, grid_height  =  np.mgrid[78:390:1, 60:98:1] #dimensioned grid [mm]
-data  =  list(map(int, data))
-data  =  sorted(data,key = int)
+"Read Coordinates and create the grid for interpolation"
+coordinates=genfromtxt(path_coordinates,skip_header=0,usecols=(1,2),delimiter=',') 
+grid_width, grid_height = np.mgrid[0:max(coordinates[:,0]):1, 0:max(coordinates[:,1]):1] #dimensioned grid [mm]
 
-spectrum  =  genfromtxt("/PATH/TO/DATA/data.csv",skip_header = 2,skip_footer = 1,usecols = 0)
-sumWatts  =  np.empty((21,1))
-rawData = []
-avData = []
+"Create length to iterate over data"
+len_dat=len(coordinates)
+
+"Initialize integrated Irradiance"
+intWatts=np.zeros((len_dat,1))
+
+"Read data directory and sort the integers"
+data=os.listdir(path_data_dirs) 
+data = list(map(int, data))
+data = sorted(data,key=int)
+
+"Read the wavelength spectrum and calculate the step between wavelength reads"
+spectrum=genfromtxt(path_data_dirs+'\\1\\1.IRR',skip_header=2,skip_footer=1,usecols=0)
+dx=spectrum[1,]-spectrum[0,]
+
+"Create length of spectrum to iterate over data"
+len_spec=len(spectrum)
+
+"Extract data from the different reads, average them and integrate the are under the curve of the averaged spectrum"
 for i in data:
-    probe = np.empty((250,5))
-    av = np.empty((250,1))
-    for j in range(0,5):   
-        probe[:,j] = genfromtxt("H:\\02_Projects\\02_PPB-GRO-Batch\\04_Experiments\\01_IrradianceMeasurment\\data\\"+str(i)+'\\'+str(j+1)+'.IRR',skip_header = 2,skip_footer = 1,usecols = 1)
-        rawData.append(probe)
-    for k in range(0,250):
-        av[k] = np.average(probe[k,:])
-        avData.append(av)
-        sumTemp = np.sum(av)
-        sumWatts[i] = sumTemp #W/m^2
-gridWatts = np.zeros((3,7))
-for l in range(0,7):
-    gridWatts[:,[l]] = sumWatts[(l*3):((l*3)+3)]
-coordsWatts = np.empty((21,3))
-for m in range(0,7):
-    coordsWatts[m*3:(m*3)+3,0] = width[m]
-    coordsWatts[m*3:(m*3)+3,1] = height[0:3]
-    coordsWatts[:,[2]] = sumWatts
-watts =  griddata((coordsWatts[:,0],coordsWatts[:,1]),sumWatts[:,0],(grid_width,grid_height),method = 'cubic')
+    probe=np.empty((len_spec,5))
+    for j in range(0,5):
+        probe[:,j]=genfromtxt(path_data_dirs+str(i)+'\\'+str(j+1)+'.IRR',skip_header=2,skip_footer=1,usecols=1)
+    rawData.append(probe)
+for k in range(0,len_dat):
+    av=np.empty((len_spec,1))
+    for l in range(0,len_spec):
+        av[l,]=np.average(rawData[k][l,:])
+    avData.append(av)
+for m in range(0,len_dat):
+    intWatts[m,0]=np.trapz(avData[m][:,0],dx=dx,axis=0)
 
-fig = plt.figure(1, (13.,7.))
-axGrid = plt.subplot(111)
-grid  =  ImageGrid(fig, 111,
-            nrows_ncols  =  (1, 1),
-            add_all = True,
-            axes_pad = 0.0,
-            label_mode = "L",
-            cbar_mode = "each",
-            cbar_location = "top",
-            cbar_pad = 0.05
+"Associate the coordinates with their integrated Irradiance reading"
+coordsWatts=np.empty((len_dat,3))
+coordsWatts[:,0]=coordinates[:,0]
+coordsWatts[:,1]=coordinates[:,1]
+coordsWatts[:,[2]]=intWatts
+
+"Interpolate the Irradiance data on the grid"
+watts= griddata((coordsWatts[:,0],coordsWatts[:,1]),coordsWatts[:,2],(grid_width,grid_height),method='cubic')
+
+"Setting up the plots:"
+fig=plt.figure(1, (13.,7.))
+axGrid=plt.subplot(111)
+grid = ImageGrid(fig, 111,
+            nrows_ncols = (1, 1),
+            add_all=True,
+            axes_pad=0.0,
+            label_mode="L",
+            cbar_mode="each",
+            cbar_location="top",
+            cbar_pad=0.05
             )
 
-ax1 = grid[0]
+ax1=grid[0]
 ax1.set_rasterization_zorder(0)
-v  =  np.linspace(0, max(sumWatts), 10, endpoint = True)
-Cof = ax1.pcolormesh(grid_width, grid_height, watts,vmin = 0.,vmax = max(sumWatts))
+v = np.linspace(0, max(intWatts), 10, endpoint=True)
+Cof=ax1.pcolormesh(grid_width, grid_height, watts,vmin=0.,vmax=max(intWatts))
 Cof.set_rasterized(True)
-ax1.set_xlabel("x [mm]", labelpad = 18)
-ax1.set_ylabel("y [mm]", labelpad = 18)
-plt.setp(axGrid, frame_on = False, xticks = (), yticks = ())
-grid.cbar_axes[0].colorbar(Cof,ticks = v)
+ax1.set_xlabel("x [mm]", labelpad=18)
+ax1.set_ylabel("y [mm]", labelpad=18)
+plt.setp(axGrid, frame_on=False, xticks=(), yticks=())
+grid.cbar_axes[0].colorbar(Cof,ticks=v)
 grid.cbar_axes[0].set_xlabel("Irradiance [W m^{-2}]")
 
-plt.savefig('powerDistribution.pdf', format = 'pdf')
+plt.savefig('powerDistribution.pdf', format='pdf')
+
+fig2=plt.figure(2, (13.,7.))
+for i in range(0,len_dat):
+    plt.plot(spectrum,avData[i][:,0],label=str(i))
+#    plt.legend()
+    plt.xlabel('Wavelength [nm]')
+    plt.ylabel('Irradiance Density [W m^{-2} nm^{-1}]')
+    plt.savefig('spec.pdf',format='pdf')
